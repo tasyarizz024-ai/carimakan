cf99883f27d036404de743614462264f75315e16
Tasya Rismala <tasyarizz024@gmail.com>
2026-07-01 08:06:03 +0700

    Fix backend api

--- FrontEnd/src/services/api.js
+++ FrontEnd/src/services/api.js
@@ -7,7 +7,8 @@
 import axios from "axios";
 
 // 🔥 PAKAI ENVIRONMENT VARIABLE
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+// 🔥 GANTI INI
+const BASE_URL = import.meta.env.VITE_API_URL || "https://carimakan-production.up.railway.app";
 
 const api = axios.create({
   baseURL: BASE_URL,
